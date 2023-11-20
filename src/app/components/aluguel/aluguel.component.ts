205301e0db6b73c47d6dc886949acc93cb2967c0
lucas teixeira <91092558+DRAVINCK@users.noreply.github.com>
2023-11-20 17:48:53 -0300

    Update aluguel.component.ts

--- src/app/components/aluguel/aluguel.component.ts
+++ src/app/components/aluguel/aluguel.component.ts
@@ -16,31 +16,47 @@ export class AluguelComponent implements OnInit {
   formulario: any;
   tituloFormulario: string = '';
   alugueis: Aluguel[] = [];
-
   constructor(private aluguelService : AluguelService,
               private clientesService : ClienteService,
               private veiculoService : VeiculoService
               ){}
-
   ngOnInit(): void {
-    this.tituloFormulario = 'Novo Aluguel';
-    this.formulario = new FormGroup({
-      id: new FormControl(null),
-      cliente: new FormControl(null),
-      clienteCpf: new FormControl(null),
-      veiculo: new FormControl(null),
-      veiculoPlaca: new FormControl(null)
-    });
-
-    this.aluguelService.listarTodos().subscribe(alugueis => {
+      this.tituloFormulario = 'Novo Aluguel';
+      this.formulario = new FormGroup({
+        id: new FormControl(null),
+        cliente: new FormControl(null),
+        clienteCpf: new FormControl(null),
+        veiculo: new FormControl(null),
+        veiculoPlaca: new FormControl(null)
+      })
+      this.listarAlugueis();
+  }
+  listarAlugueis(): void {
+    this.aluguelService.listar().subscribe(alugueis => {
       this.alugueis = alugueis;
     });
   }
-  
   enviarFormulario(): void{
     const aluguel : Aluguel = this.formulario.value;
-    this.aluguelService.cadastrar(aluguel).subscribe(result =>{
-      alert('Aluguel cadastrado')
-    })
+    if (aluguel.id) {
+      this.aluguelService.atualizar(aluguel).subscribe(result => {
+        alert('Aluguel atualizado');
+        this.listarAlugueis();
+      });
+    } else {
+      this.aluguelService.cadastrar(aluguel).subscribe(result => {
+        alert('Aluguel cadastrado');
+        this.listarAlugueis();
+      });
+    }
+  }
+  editarAluguel(aluguel: Aluguel): void {
+    this.formulario.patchValue(aluguel);
+  }
+  excluirAluguel(id: string): void {
+    this.aluguelService.excluir(id).subscribe(result => {
+      alert('Aluguel excluído');
+      this.listarAlugueis();
+    });
   }
 }
